a8df153a028fa9d580e62e1cc3247bef8787ccb4
Clint Kelly <clint@wibidata.com>
2014-01-15 14:22:41 -0800

    Update to use EmbeddedCassandraService.
    Requires JDK 1.7.

--- src/main/java/org/kiji/cassandra/App.java
+++ src/main/java/org/kiji/cassandra/App.java
@@ -1,8 +1,10 @@
 package org.kiji.cassandra;
 
 import com.datastax.driver.core.*;
+import org.apache.cassandra.service.EmbeddedCassandraService;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
 
@@ -11,24 +13,35 @@ import java.util.List;
  */
 public class App {
 
-  public static final String KEYSPACE_NAME = "demo";
-  public static final String TABLE_NAME = "users";
+  static final String KEYSPACE_NAME = "demo";
+  static final String TABLE_NAME = "users";
 
-  public static void main(String[] args) {
+  private Cluster cluster;
+  private Session session;
+
+  private static EmbeddedCassandraService cassandra;
+
+  public void setup() throws IOException {
+    cassandra = new EmbeddedCassandraService();
+    cassandra.start();
+
 
     // Connect to the cluster and open a session
-    Cluster cluster = Cluster.builder().addContactPoint("172.16.7.2").build();
-    Session session = cluster.connect();
+    //Cluster cluster = Cluster.builder().addContactPoint("172.16.7.2").build();
+    cluster = Cluster.builder().addContactPoint("localhost").build();
+
+    session = cluster.connect();
 
     // Create the keyspace and table
     session.execute("CREATE KEYSPACE IF NOT EXISTS " + KEYSPACE_NAME +
         " WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
     session.execute("CREATE TABLE IF NOT EXISTS " + KEYSPACE_NAME + "." + TABLE_NAME + " " +
         "( name text PRIMARY KEY, name_as_bytes blob );");
+  }
 
+  public void insertData(String name) {
     //----------------------------------------------------------------------------------------------
     // Insert name as String and blob, read back, and very that everything looks good!
-    String name = "Ms. Foo Bar";
 
     // Emulate what most of Kiji would do, and turn the String into byte[].
 
@@ -49,10 +62,12 @@ public class App {
         "VALUES (?,?);";
     PreparedStatement preparedStatement = session.prepare(queryText);
     session.execute(preparedStatement.bind(name, nameToWriteAsByteBuffer));
+  }
 
+  public void readAndCheckData(String name) {
     // Now read the value back out
-    queryText = "SELECT * FROM " + KEYSPACE_NAME + "." + TABLE_NAME + " WHERE name=?";
-    preparedStatement = session.prepare(queryText);
+    String queryText = "SELECT * FROM " + KEYSPACE_NAME + "." + TABLE_NAME + " WHERE name=?";
+    PreparedStatement preparedStatement = session.prepare(queryText);
     ResultSet resultSet = session.execute(preparedStatement.bind(name));
 
     // Now convert the blob into a string
@@ -73,7 +88,7 @@ public class App {
     // This is what we would pass to the normal Kiji code (that is designed to work with HBase):
     byte[] nameReadAsBlobByteArray = new byte[nameReadAsBlobByteBuffer.remaining()];
     nameReadAsBlobByteBuffer.get(nameReadAsBlobByteArray);
-    
+
     // Now convert back to a string and check
     String nameReadAsBlobString = Bytes.toString(nameReadAsBlobByteArray);
 
@@ -81,12 +96,27 @@ public class App {
       System.err.println("Problem reading back blob!");
       System.err.println("Expected to read back >:" + name +":<");
       System.err.println("Got instead >:" + nameReadAsBlobString + ":<");
-      System.err.println("Client -> table byte[] = " + nameToWriteAsByteArray);
-      System.err.println("Table -> client byte[] = " + nameReadAsBlobByteArray);
+      //System.err.println("Client -> table byte[] = " + nameToWriteAsByteArray);
+      //System.err.println("Table -> client byte[] = " + nameReadAsBlobByteArray);
     } else {
         System.out.println("Blob matches fine!");
     }
+  }
 
+  public void close() {
     cluster.shutdown();
+  }
+
+  public static void main(String[] args) {
+    App app = new App();
+    try {
+      app.setup();
+      String name = "49ers in the superbowl?!";
+      app.insertData(name);
+      app.readAndCheckData(name);
+      app.close();
+    } catch (IOException ioe) {
+      System.err.println("Could not start embedded C*!");
+    }
   }
 }
